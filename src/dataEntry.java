551c3c2347d993fc8dd2e2523601938ac56ea5be
gusBrasil <gustavomedbra@gmail.com>
2022-05-02 17:12:11 -0300

    Update

---
+++ src/dataEntry.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class dataEntry {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int x;
+        x = sc.nextInt();
+
+        System.out.println("Você digitou: " + x);
+
+        sc.close();
+    }
+}
