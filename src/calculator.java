9a88555485ef22fbbd20facd0d12b07158d18343
gusBrasil <gustavomedbra@gmail.com>
2022-05-03 09:53:01 -0300

    Update

--- src/calculator.java
+++ src/calculator.java
@@ -3,24 +3,37 @@ import java.util.Scanner;
 
 public class calculator {
     public static void main(String[] args){
-        System.out.println("Bem-vindo a sua nova calculadora!");
+        System.out.print("Bem-vindo a sua nova calculadora!");
         Scanner sc = new Scanner(System.in);
 
-        String input;
-
+        int input = sc.nextInt();
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        input = sc.next("Deseja realizar uma soma, subtração, multiplicação ou divisão? ");
-        if (Objects.equals(input, "soma")) {
+        if (input == 1) {
             soma(a, b);
+        } else if (input == 2) {
+            subtrai(a, b);
+        } else if (input == 3) {
+            multiplica(a, b);
         }
 
 
         sc.close();
     }
 
-    public static int soma(int x, int y) {
-        System.out.println(x + y);
+    public static void soma(int x, int y) {
+        int z = x +y;
+        System.out.println(z);
+    }
+
+    public static void subtrai(int x, int y) {
+        int z = x - y;
+        System.out.println(z);
+    }
+
+    public static void multiplica(int x, int y) {
+        int z = x * y;
+        System.out.println(z);
     }
 }
