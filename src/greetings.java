71a5b7f7dde6b51fd5f5b4624b261fa9071bc08a
gusBrasil <gustavomedbra@gmail.com>
2022-05-02 22:09:15 -0300

    update

---
+++ src/greetings.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class greetings {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int hora;
+
+        System.out.println("Que horas são?");
+        hora = sc.nextInt();
+
+        if(hora < 12) {
+            System.out.println("Bom dia!");
+        } else if (hora < 18) {
+            System.out.println("Boa tarde!");
+        } else if (hora > 18) {
+            System.out.println("Boa noite!");
+        }
+    }
+}
